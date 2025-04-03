4e8ecd5735caa2a905b76a7bc8a7cde909b73a39
s1mba93 <danielelson93@gmail.com>
2025-04-03 16:52:02 +0100

    Split word count function to new file

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
+from stats import get_word_count
+
 def get_book_text(filepath):
     file_contents = None
     with open(filepath) as f:
         file_contents = f.read()
         
     return file_contents
-
-
-def get_word_count(filepath):
-    file_contents = None
-    word_count = 0
-    with open(filepath) as f:
-        file_contents = f.read()
-    
-    file_contents = file_contents.split()
-    for word in file_contents:
-        word_count += 1
-        
-    return f"{word_count} words found in the document" 
 
 
 def main():
---
+++ stats.py
@@ -0,0 +1,11 @@
+def get_word_count(filepath):
+    file_contents = None
+    word_count = 0
+    with open(filepath) as f:
+        file_contents = f.read()
+    
+    file_contents = file_contents.split()
+    for word in file_contents:
+        word_count += 1
+        
+    return f"{word_count} words found in the document" 
